88e7925932bf9528bcadcd3d9213ae056ed95c41
afiliate12 <purwantoabeee@gmail.com>
2026-04-02 11:15:16 +0700

    Create music.js

---
+++ music.js
@@ -0,0 +1,12 @@
+const musicList = [
+    { id: 1, title: "Berita hari ini", artist: "Cards Id Purwanto", duration: "2:24", url: "https://www.dropbox.com/scl/fi/cxlidxlki5qd8y7e4graw/1.mp3?rlkey=p0r4arad6tpk1iy1fc2godslj&st=jszqap1h&dl=1" },
+    { id: 2, title: "Kata kata indah hari ini", artist: "Cards Id Purwanto", duration: "3:10", url: "https://www.dropbox.com/scl/fi/p8x93iv5x8utru8yhf9g4/2.mp3?rlkey=5xis8n9u92mjw7do1fbunen9f&st=wjozt2ru&dl=1" },
+    { id: 3, title: "Tutorial terbaik khusus hari ini", artist: "Cards Id Purwanto", duration: "4:05", url: "https://www.dropbox.com/scl/fi/1n43dgeaa0qxp8lfn0xih/3.mp3?rlkey=3au43htu4y9li7ge9fkqh4v8x&st=srvxl8hs&dl=1" },
+    { id: 4, title: "Kata mutiara hari ini", artist: "Cards Id Purwanto", duration: "3:45", url: "https://www.dropbox.com/scl/fi/krjo7atz5pyzglqbzetyd/4.mp3?rlkey=vptqbobawgq6f7r7h4ca27g2v&st=kz23xejn&dl=1" },
+    { id: 5, title: "Lagu pop indonesia", artist: "Cards Id Purwanto", duration: "2:58", url: "https://www.dropbox.com/scl/fi/we3cwmv2wyfrv55limm6s/5.mp3?rlkey=3lfaqdmljefiij8w51rbj2ctv&st=wpl6fks6&dl=1" },
+    { id: 6, title: "Lagu dangdut indonesia", artist: "Cards Id Purwanto", duration: "3:15", url: "https://www.dropbox.com/scl/fi/wwir4ykfx03p8tgibdnh4/6.mp3?rlkey=6owrkxk6qkol04gljy15epxcz&st=mvrsnm8d&dl=1" },
+    { id: 7, title: "Lagu anak anak", artist: "Cards Id Purwanto", duration: "5:20", url: "https://www.dropbox.com/scl/fi/vzx5zzayq3ot8m72uxdsz/7.mp3?rlkey=p1ba6alm2l3lnpld7svkfm9h9&st=vtopkum5&dl=1" },
+    { id: 8, title: "Lagu pilihan islami", artist: "Cards Id Purwanto", duration: "3:30", url: "https://www.dropbox.com/scl/fi/89ng9x5q1edwlj2cuzhzp/8.mp3?rlkey=nu1fwx18j2yz0vg2aeercu5qw&st=r4wewi0x&dl=1" },
+    { id: 9, title: "Koleksi lagu luar negeri", artist: "Cards Id Purwanto", duration: "4:12", url: "https://www.dropbox.com/scl/fi/i5q2acj6k8zna1ld808db/9.mp3?rlkey=gwq9fgctt3tl2xpt0gimm2yhl&st=xy3mc94r&dl=1" },
+    { id: 10, title: "Pidato singkat khusus hari ini", artist: "Cards Id Purwanto", duration: "2:50", url: "https://www.dropbox.com/scl/fi/0ab6k2rbq6laien92lf61/10.mp3?rlkey=q63n4rlle0nb7wi4dzvb2vvm8&st=bl9a2ych&dl=1" }
+];
